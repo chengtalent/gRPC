0252b1fa8bb94b0ede30bc4500f1ee31d7e890a2
Silei Cheng <silei.cheng@dianrong.com>
2016-10-13 11:29:48 +0800

    TCP read write and certificate verify is OK

--- mynet/client/client.go
+++ mynet/client/client.go
@@ -3,29 +3,73 @@ package main
 import (
 	"net"
 	"bufio"
-	"os"
 	"fmt"
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/proto"
+	"context"
 )
+
+const (
+	address     = "localhost:50051"
+
+	pub = `-----BEGIN CERTIFICATE-----
+MIIB3DCCAYGgAwIBAgIBATAKBggqhkjOPQQDAzBXMR8wHQYDVQQGExZwa2kuY2Eu
+c3ViamVjdC5jb3VudHJ5MSQwIgYDVQQKExtwa2kuY2Euc3ViamVjdC5vcmdhbml6
+YXRpb24xDjAMBgNVBAMTBVNpbGVpMB4XDTE2MTAxMjA5Mzc1OVoXDTE3MDExMDA5
+Mzc1OVowVTEfMB0GA1UEBhMWcGtpLmNhLnN1YmplY3QuY291bnRyeTEkMCIGA1UE
+ChMbcGtpLmNhLnN1YmplY3Qub3JnYW5pemF0aW9uMQwwCgYDVQQDEwNwdWIwWTAT
+BgcqhkjOPQIBBggqhkjOPQMBBwNCAATOpHXnym0DPbc+waIp4ABB92BpQ/fWPuHI
+7iWpz5vWtTCwJb0QPk/3jKyyWDp7vkxPpQIpgupFiUivds6p9PnXo0AwPjAOBgNV
+HQ8BAf8EBAMCAoQwDAYDVR0TAQH/BAIwADANBgNVHQ4EBgQEAQIDBDAPBgNVHSME
+CDAGgAQBAgMEMAoGCCqGSM49BAMDA0kAMEYCIQCBKU6vWfMdzydughFM/FuAvqG6
+CXqK8o6NaKZrfWxhAgIhAKXG2v0BgXTqdoJo/RS1gUDWnUFYDibMcNLne8O4f7Ry
+-----END CERTIFICATE-----`
+)
+
+func issueCertificate() []byte{
+	// Set up a connection to the server.
+	conn1, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		fmt.Println("did not connect: %v", err)
+	}
+	defer conn1.Close()
+
+	c3 := pb.NewCAClient(conn1)
+	r3, err3 := c3.IssueCertificate(context.Background(), &pb.CertificateRequest{In:[]byte(pub), Name:"pub"})
+	if err3 != nil {
+		fmt.Println("could not IssueCertificate: %v", err3)
+	}
+	fmt.Println("IssueCertificate: %s", r3.In)
+
+	return r3.In
+}
 
 func main() {
 
+
 	// connect to this socket
 	conn, _ := net.Dial("tcp", "127.0.0.1:8081")
-	for {
+	{
 		// read in input from stdin
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Text to send: ")
-		text, _ := reader.ReadString('\n')
+		//reader := bufio.NewReader(os.Stdin)
+		//fmt.Print("Text to send: ")
+		//text, _ := reader.ReadString('\n')
 
 		// send to socket
 		//fmt.Fprintf(conn, text + "\n")
 
-		if _, err := conn.Write([]byte(text + "\n")); err != nil {
-			continue
+		fmt.Println("begin Write message")
+
+		//cert := issueCertificate()
+
+		if _, err := conn.Write([]byte(pub)); err != nil {
+			fmt.Println(err)
 		}
+
+		fmt.Println("end Write message")
 
 		// listen for reply
 		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Print("Message from server: "+message)
+		fmt.Println("Message from server: "+message)
 	}
 }
--- mynet/server/server.go
+++ mynet/server/server.go
@@ -3,29 +3,121 @@ package main
 import (
 	"fmt"
 	"net"
+
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/helloworld/proto"
+	"context"
+	//"io/ioutil"
+	"io"
+	"bytes"
+	//"strings"
 	"bufio"
-	"strings"
 )
+
+const (
+	address     = "localhost:50051"
+
+	Pub = `-----BEGIN ECDSA PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEzqR158ptAz23PsGiKeAAQfdgaUP3
+1j7hyO4lqc+b1rUwsCW9ED5P94ysslg6e75MT6UCKYLqRYlIr3bOqfT51w==
+-----END ECDSA PUBLIC KEY-----`
+)
+
+func readFully(conn net.Conn) ([]byte, error) {
+
+	result := bytes.NewBuffer(nil)
+	var buf [512]byte
+
+	for {
+		n, err := conn.Read(buf[0:])
+		result.Write(buf[0:n])
+
+		fmt.Println(n)
+
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+
+			return nil, err
+		}
+	}
+
+	return result.Bytes(), nil
+}
+
+func getCACertificate() []byte{
+	// Set up a connection to the server.
+	conn1, err := grpc.Dial(address, grpc.WithInsecure())
+
+	if err != nil {
+		fmt.Println("did not connect: %v", err)
+	}
+	defer conn1.Close()
+
+	c3 := pb.NewCAClient(conn1)
+
+	r3, err3 := c3.GetCACertificate(context.Background(), &pb.NoParam{})
+	if err3 != nil {
+		fmt.Println("could not GetCACertificate: %v", err3)
+	}
+	fmt.Println("GetCACertificate: %s", r3.In)
+
+	return r3.In
+}
+
+func verifySignature(cert, root []byte) bool {
+	conn1, err := grpc.Dial(address, grpc.WithInsecure())
+
+	if err != nil {
+		fmt.Println("did not connect: %v", err)
+	}
+	defer conn1.Close()
+
+	c3 := pb.NewCAClient(conn1)
+
+	r4, err4 := c3.VerifySignature(context.Background(), &pb.CertificateData{Cert:cert, Root:root})
+	if err4 != nil {
+		fmt.Println("could not VerifySignature: %v", err4)
+	}
+
+	fmt.Println("VerifySignature: %s", r4.Valid)
+
+	return r4.Valid
+}
 
 func main() {
 
 	fmt.Println("Launching server...")
 
-	// listen on all interfaces
-	ln, _ := net.Listen("tcp", ":8081")
 
-	// accept connection on port
+	ln, _ := net.Listen("tcp", "127.0.0.1:8081")
 	conn, _ := ln.Accept()
 
-	// run loop forever (or until ctrl-c)
-	for {
-		// will listen for message to process ending in newline (\n)
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		// output message received
-		fmt.Print("Message Received:", string(message))
-		// sample process for string received
-		newmessage := strings.ToUpper(message)
-		// send new string back to client
-		conn.Write([]byte(newmessage + "\n"))
+	{
+		fmt.Print("begin read message")
+
+
+		//message, _ := bufio.NewReader(conn).ReadString('\n')
+		message := make([]byte, 1000)
+		n, err := bufio.NewReader(conn).Read([]byte(message))
+		if err != nil {
+			fmt.Println("could not readfull: %v", err)
+		}
+		fmt.Println(n)
+
+		msg := message[0:n]
+		fmt.Println("Message Received:", string(msg))
+
+		root := getCACertificate()
+		valid := verifySignature(msg, root)
+
+
+		if valid {
+			conn.Write([]byte("certificate pass"))
+		}else {
+			conn.Write([]byte("certificate failed"))
+		}
+
 	}
 }
